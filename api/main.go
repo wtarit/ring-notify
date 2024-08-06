13f873fd452429b3b6a41d49225ce61ebca110cc
wtarit <60599564+wtarit@users.noreply.github.com>
2024-08-07 02:29:13 +0700

    Add Handler func for Vercel

--- api/main.go
+++ api/main.go
@@ -8,12 +8,24 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
+func initEcho() *echo.Echo {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World")
 	})
 	e.POST("/notify/call", notify.Call)
 	e.POST("/user/create", user.CreateUser)
+	return e
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e := initEcho()
+	e.ServeHTTP(w, r)
+}
+
+func main() {
+	// http.HandleFunc("/*", Handler)
+	// log.Fatal(http.ListenAndServe(":1323", nil))
+	e := initEcho()
 	e.Logger.Fatal(e.Start(":1323"))
 }
