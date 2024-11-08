480aaca2294eeba23ed4ab91231a81803005914a
wtarit <60599564+wtarit@users.noreply.github.com>
2024-11-07 17:05:32 -0800

    Remove handler and move initialization of echo to main

--- api/main.go
+++ api/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"api/handler"
+	"api/configs"
+	"api/notify"
+	"api/user"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	// http.HandleFunc("/*", Handler)
-	// log.Fatal(http.ListenAndServe(":1323", nil))
-	e := handler.Init()
+	configs.InitFirebase()
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World")
+	})
+	e.POST("/notify/call", notify.Call)
+	e.POST("/user/create", user.CreateUser)
 	e.Logger.Fatal(e.Start(":1323"))
 }
