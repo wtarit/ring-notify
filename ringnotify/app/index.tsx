560132241155b3a6a03cbfd96be3d444ad4220ee
wtarit <60599564+wtarit@users.noreply.github.com>
2025-04-24 02:55:34 -0700

    Cleanup logic on main page

--- ringnotify/app/index.tsx
+++ ringnotify/app/index.tsx
@@ -1,40 +1,17 @@
 import { useEffect } from "react";
 import { Text, View } from "react-native";
-import messaging from '@react-native-firebase/messaging'
-import CallScreen from '@/lib/callscreen';
-import RNCallKeep from 'react-native-callkeep';
+import messaging from "@react-native-firebase/messaging";
+import CallScreen from "@/lib/callscreen";
 
 export default function Index() {
   useEffect(() => {
-    // const options = {
-    //   ios: {
-    //     appName: 'Ring Notify',
-    //   },
-    //   android: {
-    //     alertTitle: 'Permissions required',
-    //     alertDescription:
-    //       'This application needs to access your phone accounts',
-    //     cancelButton: 'Cancel',
-    //     okButton: 'ok',
-    //     imageName: 'phone_account_icon',
-    //     // additionalPermissions: [PermissionsAndroid.PERMISSIONS.example],
-    //     additionalPermissions: [],
-    //     // Required to get audio in background when using Android 11
-    //     foregroundService: {
-    //       channelId: 'me.wtarit.ringnotify',
-    //       channelName: 'Foreground service for my app',
-    //       notificationTitle: 'My app is running on background',
-    //       notificationIcon: 'Path to the resource icon of the notification',
-    //     },
-    //   },
-    // };
     CallScreen.setupCallKeep();
   }, []);
 
   useEffect(() => {
-    const unsubscribe = messaging().onMessage(remoteMessage => {
+    const unsubscribe = messaging().onMessage((remoteMessage) => {
       if (remoteMessage.data != undefined) {
-        console.log("foregound call")
+        console.log("foregound call");
         CallScreen.displayIncomingCall(remoteMessage.data.text);
       }
     });
@@ -47,7 +24,7 @@ export default function Index() {
   useEffect(() => {
     async function getFCMtoken(): Promise<string> {
       const token = await messaging().getToken();
-      console.log('token', token);
+      console.log("token", token);
       return token;
     }
     getFCMtoken();
