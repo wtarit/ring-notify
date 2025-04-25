4861a48957e563386654ba81b2e3d69ff1d790b7
wtarit <60599564+wtarit@users.noreply.github.com>
2025-04-24 23:53:17 -0700

    Replace placeholder with actual message from API on call screen

--- ringnotify/lib/fcm.ts
+++ ringnotify/lib/fcm.ts
@@ -1,7 +1,10 @@
-import {FirebaseMessagingTypes} from '@react-native-firebase/messaging'
-import CallScreen from './callscreen'
+import { FirebaseMessagingTypes } from "@react-native-firebase/messaging";
+import CallScreen from "./callscreen";
 
-export const callHandler = async (message: FirebaseMessagingTypes.RemoteMessage) => {
-  console.log("message =", message);
-  CallScreen.displayIncomingCall("test");
-}
+export const callHandler = async (
+  message: FirebaseMessagingTypes.RemoteMessage
+) => {
+  if (message.data?.text) {
+    CallScreen.displayIncomingCall(message.data.text as string);
+  }
+};
